5637de4be3329e382efcdac56dd718539c1406fc
Aroma-oh <115550622+Aroma-oh@users.noreply.github.com>
2023-08-09 01:38:15 +0900

    feat: 그리드 스타일 컴포넌트 생성

---
+++ grid/src/component/Grid.tsx
@@ -0,0 +1,24 @@
+import { ReactNode } from 'react';
+import styled from 'styled-components';
+
+interface GridProps {
+  mt?: number;
+  mb?: number;
+  p?: number;
+  children: ReactNode;
+}
+
+export const Grid = ({ mt, mb, p, children }: GridProps) => {
+  return (
+    <GridStyled mt={mt} mb={mb} p={p}>{children}</GridStyled>
+  )
+}
+
+const GridStyled = styled.section<GridProps>`
+  display: grid;
+  gap: 25px;
+  grid-template-columns: repeat(6, 1fr);
+  margin-top: ${({ mt }) => mt ? `${mt}px` : `25px`};
+  margin-bottom: ${({ mb }) => mb ? `${mb}px` : `25px`};
+  padding: ${({ p }) => p ? `0 ${p}px` : `0 110px`};
+`
